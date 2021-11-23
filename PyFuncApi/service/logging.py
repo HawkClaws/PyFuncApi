91e0015de294f76dd7cb65f712c73744941fa190
HawkClaws <laspid07@gmail.com>
2021-11-23 11:38:21 +0900

    Loggingのパス階層を改善 Codeが見つからない場合の処理を修正

--- PyFuncApi/service/logging.py
+++ PyFuncApi/service/logging.py
@@ -1,3 +1,4 @@
+import traceback
 from django.http.request import HttpRequest
 from ..repository.repository import Repository
 import json
@@ -10,12 +11,17 @@ def logging(request: HttpRequest, url: str, body: str):
     try:
         repository = Repository(
             settings.LOGGING_REPOSITORY_URL)
+        now = datetime.now()
+        hour = now.strftime("%Y-%m-%d")
+        minute = now.strftime("%H:%M:%S")
 
-        repository.insert('', json.dumps({
+        path = '/'.join([url.replace("/","_"),request.method, hour, minute])
+        repository.update(path, json.dumps({
             "url": url,
             "body": body,
             "client_addr": str(request.META.get('REMOTE_ADDR')),
             "datetime": str(datetime.now())
         }))
-    except:
+    except Exception as e:
+        print(traceback.format_exc())
         pass
--- PyFuncApi/views.py
+++ PyFuncApi/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.http.request import HttpRequest
 import traceback
-import json
-from datetime import datetime
 from .service.api_crud_service import api_operation_common, get_code_data
 from .service.logging import logging
 from .service.exec_service import exec_async_wrapper, exec_wrapper, api_exec_async_result, HTTP_RESPONSE
@@ -36,8 +34,11 @@ def api_exec(request: HttpRequest, url: str):
         input = request.body.decode('utf-8')
         logging(request, url, input)
 
-        code = get_code_data(url)["code"]
-        res = exec_wrapper(code, input)[HTTP_RESPONSE]
+        code_info = get_code_data(url)
+        if code_info is None:
+            return HttpResponse("Can not find api url:"+url)
+
+        res = exec_wrapper(code_info["code"], input)[HTTP_RESPONSE]
         res['Content-Type'] = 'text/json'
         return res
     except Exception as e:
